0944cd73eee98dcfa50b1b4fca1a0606213b0885
Gene Kogan <kogan.gene@gmail.com>
2015-03-22 18:58:39 +0700

    removed superfluous control2

--- Control2/src/GuiTextBox.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  GuiTextBox.cpp
-//  template_osc
-//
-//  Created by Eugene Kogan on 3/13/15.
-//
-//
-
-#include "GuiTextBox.h"
--- Control2/src/OscPanel.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  OscPanel.cpp
-//  template_osc
-//
-//  Created by Eugene Kogan on 3/12/15.
-//
-//
-
-#include "OscPanel.h"
