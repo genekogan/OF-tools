fe358cc2983b5b7bc463db500e7650b197014328
Gene <gene@dyn-129-236-232-156.dyn.columbia.edu>
2015-04-11 22:15:47 -0400

    remove extraneous control version

--- _Control2/src/GuiMultiRangeSlider.cpp
+++
@@ -1,9 +0,0 @@
-#include "GuiMultiRangeSlider.h"
-
-
-GuiMultiRangeSliderBase::GuiMultiRangeSliderBase(string name) : GuiMultiElement(name)
-{
-    marginY = GUI_DEFAULT_MARGIN_Y_INNER;
-    setHeader(getName());
-}
-
