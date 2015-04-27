9ed01584cfb36413eb16e49fa7ec101d3aff4546
Gene <gene@dyn-129-236-232-208.dyn.columbia.edu>
2015-04-26 21:34:43 -0400

    removed extra Control

--- Control2/src/Base64.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-
-std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len);
-std::string base64_decode(std::string const& encoded_string);
-
--- Control2/src/GuiMultiSlider.cpp
+++
@@ -1,8 +0,0 @@
-#include "GuiMultiSlider.h"
-
-
-GuiMultiSliderBase::GuiMultiSliderBase(string name) : GuiMultiElement(name)
-{
-    marginY = GUI_DEFAULT_MARGIN_Y_INNER;
-    setHeader(getName());
-}
